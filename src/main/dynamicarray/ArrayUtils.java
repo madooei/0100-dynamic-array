15028c81b220dfdc4809271ca9baf4f51f25dbc8
Ali Madooei <madooei@jhu.edu>
2026-08-10 22:54:25 -0400

    Update from playground

--- src/main/dynamicarray/ArrayUtils.java
+++ src/main/dynamicarray/ArrayUtils.java
@@ -13,26 +13,26 @@ public final class ArrayUtils {
   }
 
   // Returns the index of the smallest element in a[from..size).
-  public static int indexOfMin(DynamicArray a, int from) {
+  public static int indexOfMin(DynamicArray list, int from) {
     int minIndex = from;
-    for (int i = from + 1; i < a.size(); i++) {
-      if (a.get(i) < a.get(minIndex)) {
+    for (int i = from + 1; i < list.size(); i++) {
+      if (list.get(i) < list.get(minIndex)) {
         minIndex = i;
       }
     }
     return minIndex;
   }
 
-  public static void swap(DynamicArray a, int i, int j) {
-    int temp = a.get(i);
-    a.set(i, a.get(j));
-    a.set(j, temp);
+  public static void swap(DynamicArray list, int i, int j) {
+    int temp = list.get(i);
+    list.set(i, list.get(j));
+    list.set(j, temp);
   }
 
-  public static void selectionSort(DynamicArray a) {
-    for (int i = 0; i < a.size() - 1; i++) {
-      int min = indexOfMin(a, i);  // find the smallest remaining element
-      swap(a, i, min);             // move it into position i
+  public static void selectionSort(DynamicArray list) {
+    for (int i = 0; i < list.size() - 1; i++) {
+      int min = indexOfMin(list, i);  // find the smallest remaining element
+      swap(list, i, min);             // move it into position i
     }
   }
 }
